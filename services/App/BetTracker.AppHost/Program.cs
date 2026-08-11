776fec6bed5d063bbf92b2680afecc6edcfc00b9
gzaxx <gzaxxor@gmail.com>
2026-08-11 19:31:10 +0200

    Fixed Aspire dashboard

--- services/App/BetTracker.AppHost/Program.cs
+++ services/App/BetTracker.AppHost/Program.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.BetTracker_ApiService>("api");
+var api = builder
+    .AddProject<Projects.BetTracker_ApiService>("api")
+    .WithExternalHttpEndpoints();
+
+builder
+    .AddViteApp("frontend", "../../../apps/bet-tracker-client")
+    .WithReference(api)
+    .WithEnvironment("VITE_API_PROXY_TARGET", api.GetEndpoint("http"))
+    .WaitFor(api)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
